1552dd97b1d6273762f2fc0a95e13c1a63b2c6e0
GustavoCar217 <GustavoVieira217@hotmail.com>
2024-09-12 22:02:36 -0300

    Add files via upload

---
+++ exercícios/exercicioViagem.py
@@ -0,0 +1,5 @@
+import math
+velocidade = float(input("Velocidade média do veículo?: "))
+distancia = float(input("Distância à Percorrer?: "))
+resultado = (round(distancia / velocidade))
+print("Tempo de viagem de",resultado,"horas")
