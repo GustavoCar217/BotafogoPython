b483cb21c609ef963a8f98ff19a57d131477e470
PROGRAMAÇÃO EM PYTHON - 2024.4 <47129532024.4@senacrjedu.local>
2024-12-02 11:53:12 -0300

    atualização aula 31

--- python/aula-31/techNova/core/views.py
+++ python/aula-31/techNova/core/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-servicos = [
+servicosLista = [
     {"nome": "Desenvolvimento Web", "descricao": "Criação de sites e apps", "preco": 2700.00},
     {"nome": "Consultoria", "descricao": "Análise de sistemas", "preco": 2000.00},
     {"nome": "Suporte Técnico", "descricao": "Suporte remoto e presencial", "preco": 800.00},
 ]
 
-cursos = [
+cursosLista = [
     {"nome": "Python Básico", "descricao": "Introdução à programação em Python", "carga_horaria" : 60, "preco": 1800.00},
     {"nome": "Django Completo", "descricao": "Desenvolvimento web com Django", "carga_horaria" : 80, "preco": 1500.00},
     {"nome": "Banco de Dados", "descricao": "Gestão de dados com SQL", "carga_horaria" : 50, "preco": 1200.00},
